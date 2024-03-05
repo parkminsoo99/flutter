5bdca2768b658daa92b7ab8883febae46956f48a
parkminsoo99 <zzangorc99@naver.com>
2024-03-05 20:37:56 +0900

    cf_tube 및 image_editor 제작 완성

---
+++ cf_tube/lib/const/api.dart
@@ -0,0 +1,3 @@
+const API_KEY='AIzaSyB14wcHksA_HRW7SqSml2qJ_G8f2QZWas0';
+const YOUTUBE_API_BASE_URL ='https://youtube.googleapis.com/youtube/v3/search';
+const CF_CHANNEL_ID = 'UCxZ2AlaT0hOmxzZVbF_j_Sw';
